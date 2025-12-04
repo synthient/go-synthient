dad67c451403a52f4f056fad3b27ed404b360c33
Matt Gleich <git@mattglei.ch>
2025-12-03 20:57:33 -0500

    feat: get a given ip

---
+++ _examples/ip/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/synthient/go-synthient"
+)
+
+func main() {
+	client := synthient.NewClient(os.Getenv("SYNTHIENT_API_KEY"))
+
+	resp, err := client.GetIP("213.149.183.127", nil)
+	if err != nil {
+		log.Fatalf("failed to get ip address: %s", err)
+	}
+	fmt.Println(resp.IP)
+}
